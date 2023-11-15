48cc04c3e9071fb18226dfe9cdd62a5327f4724a
Annisa Samanhudi <145771445+Annisacht@users.noreply.github.com>
2023-11-15 22:45:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 7600
+const cors = require('cors')
+const morgan = require('morgan')
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, function() {
+    console.log(`Server is running in port : ${ port }`)
+})
+
+const ConnectionMongoDB = require('./models/ConnectionMongoDB')
+ConnectionMongoDB()
+
