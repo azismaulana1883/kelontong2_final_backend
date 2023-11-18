b5eb608e1e352cc088f40f29ed85f426cf984f0a
azismaulana1883 <azis.maulana1883@gmail.com>
2023-11-18 07:35:54 +0700

    penambahan forgot password function and routing

--- Controllers/auth/auth.js
+++ Controllers/auth/auth.js
@@ -1,7 +1,135 @@
 const Cryptr = require('cryptr');
 const CryptrNew = new Cryptr('secret-key-access');
+const nodemailer = require('nodemailer');
 const JWT = require('jsonwebtoken');
 const UserModelsMongo = require('../../models/scheme/User');
+
+// Konfigurasi transporter untuk Nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'indra.kurniawan1433@gmail.com',
+        pass: 'ubpnqeblbwjmoipk'
+    }
+});
+
+function sendResetPasswordEmail(email, resetPasswordToken) {
+    const mailOptions = {
+        from: 'indra.kurniawan1433@gmail.com',
+        to: email,
+        subject: 'Reset Password',
+        text: `Click the following link to reset your password: http://localhost:5173/reset-password?email=${email}&token=${resetPasswordToken}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('Reset password email sent: ' + info.response);
+        }
+    });
+}
+
+function generateResetPasswordExpiration() {
+    const expiration = new Date();
+    expiration.setHours(expiration.getHours() + 1); // Set expiration time (e.g., 1 hour)
+    return expiration;
+}
+
+async function resetPassword(req, res, next) {
+    const { email, token, newPassword } = req.body;
+
+    try {
+        let user = await UserModelsMongo.findOne({ email: email, resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+
+        if (!user) {
+            console.error('Invalid reset password token or email:', email, token);
+            return res.status(400).send({
+                message: 'Invalid reset password token!',
+                success: false,
+                statusCode: 400
+            });
+        }
+
+        // Update password dan hapus token reset password setelah berhasil reset
+        await UserModelsMongo.updateOne(
+            { email: email },
+            { $set: { password: CryptrNew.encrypt(newPassword) }, $unset: { resetPasswordToken: 1, resetPasswordExpires: 1 } }
+        );
+
+        console.log('Password reset successful:', email, token);
+        res.status(200).send({
+            message: 'Password reset successful!',
+            success: true,
+            statusCode: 200
+        });
+    } catch (error) {
+        console.error('Error in resetPassword:', error);
+        res.status(500).send({
+            message: 'Internal Server Error',
+            success: false,
+            statusCode: 500
+        });
+    }
+}
+
+async function forgotPassword(req, res, next) {
+    const { email } = req.body;
+
+    try {
+        let user = await UserModelsMongo.findOne({ email: email });
+
+        if (!user) {
+            console.error('User not found for email:', email);
+            return res.status(404).send({
+                message: 'User not found!',
+                success: false,
+                statusCode: 404
+            });
+        }
+
+        // Generate reset password token
+        const resetPasswordToken = generateResetPasswordToken();
+
+        // Set expiration time (e.g., 1 hour)
+        const resetPasswordExpires = generateResetPasswordExpiration();
+
+        // Update user with reset password token and expiration time
+        await UserModelsMongo.updateOne(
+            { email: email },
+            { $set: { resetPasswordToken: resetPasswordToken, resetPasswordExpires: resetPasswordExpires } }
+        );
+
+        // Kirim email reset password
+        sendResetPasswordEmail(email, resetPasswordToken);
+
+        res.status(200).send({
+            message: 'Reset password email sent! Check your email for further instructions.',
+            success: true,
+            statusCode: 200
+        });
+    } catch (error) {
+        console.error('Error in forgotPassword:', error);
+        res.status(500).send({
+            message: 'Internal Server Error',
+            success: false,
+            statusCode: 500
+        });
+    }
+}
+
+// Fungsi untuk menghasilkan token reset password unik
+function generateResetPasswordToken() {
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let token = '';
+
+    for (let i = 0; i < 32; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters[randomIndex];
+    }
+
+    return token;
+}
 
 async function Login(req, res, next) {
     const { email } = req.body;
@@ -77,5 +205,8 @@ async function Login(req, res, next) {
 
 
 module.exports = {
-    Login
+    Login,
+    forgotPassword,
+    resetPassword,
+    generateResetPasswordToken
 };
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -5,6 +5,7 @@ const authController =  require('../../Controllers/auth/auth')
 
 //register user or owner
 router.post('/login', authController.Login);
-
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
