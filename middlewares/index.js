7842c8173efc1f10cb67aeac965904556a527c62
azismaulana1883 <azis.maulana1883@gmail.com>
2023-11-27 12:05:39 +0700

    penambahan akses hanya untuk owner

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,44 +1,55 @@
-const JWT = require('jsonwebtoken')
+const JWT = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-    if ( !req.headers.authorization ) {
-        res.status(401).send({
+    if (!req.headers.authorization) {
+        return res.status(401).json({
             message: 'Unauthorized!',
             statusMessage: 'Unauthorized!',
             statusCode: 401
-        })
-    } else {
-        let token = req.headers.authorization.split(' ')
-        if ( token[0].toLowerCase() !== 'bearer' ) {
-            res.status(401).send({
-                message: 'Unauthorized!',
-                statusMessage: 'Unauthorized!',
-                statusCode: 401
-            })
-        } else {
-            req.tokenVerify = token[1]
-            next()
-        }
+        });
     }
+
+    const token = req.headers.authorization.split(' ');
+
+    if (token[0].toLowerCase() !== 'bearer') {
+        return res.status(401).json({
+            message: 'Unauthorized!',
+            statusMessage: 'Unauthorized!',
+            statusCode: 401
+        });
+    }
+
+    req.tokenVerify = token[1];
+    next();
 }
 
 function verifyJWTToken(req, res, next) {
-    let token = req.tokenVerify
-    let tokenVerify = JWT.verify(token, 'secret-key-access')
+    const token = req.tokenVerify;
 
-    if ( !tokenVerify ) {
-        res.status(401).send({
+    try {
+        const tokenVerify = JWT.verify(token, 'secret-key-access');
+
+        if (!tokenVerify || tokenVerify.data.role !== 'owner') {
+            return res.status(403).json({
+                message: 'Insufficient permissions!',
+                statusMessage: 'Forbidden!',
+                statusCode: 403
+            });
+        }
+
+        req.tokenUser = tokenVerify.data;
+        next();
+    } catch (error) {
+        console.error('Error during JWT verification:', error);
+        return res.status(401).json({
             message: 'Unauthorized!',
             statusMessage: 'Unauthorized!',
             statusCode: 401
-        })
-    } else {
-        req.tokenUser = tokenVerify
-        next()
+        });
     }
 }
 
 module.exports = {
-    verifyToken, 
+    verifyToken,
     verifyJWTToken,
-}
+};
