fd9a43225d78dc2d2b7d58712f1a192480ee6b85
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-30 16:37:15 +0700

    Add routes for checkout, payment, and shipping

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 
-//Auth 
+//Auth
 const Auth = require('./auth/index')
 routes.use('/auth', Auth)
 
@@ -12,5 +12,17 @@ routes.use('/products', productRoutes);
 // Route for upload
 const uploadRoutes = require('./upload/uploadRoutes');
 routes.use('/upload', uploadRoutes);
+
+// Route for checkout
+const checkoutRoutes = require('./checkout/checkoutRoutes');
+routes.use('/checkout', checkoutRoutes);
+
+// Route for payment
+const paymentRoutes = require('./payment/paymentRoutes');
+routes.use('/payment', paymentRoutes);
+
+// Routes for shipping
+const shippingRoutes = require('./shipping/shippingRoutes');
+routes.use('/shipping', shippingRoutes);
 
 module.exports=routes
